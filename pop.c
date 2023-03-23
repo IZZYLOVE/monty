7c69f8d1e4357be3f8ce4e225a790b23e0c323eb
solokool66 <solokool66@gmail.com>
2023-03-23 06:55:52 -0700

    correction of pop.c task

--- pop.c
+++ pop.c
@@ -11,13 +11,13 @@ stack_t *h;
 
 if (*head == NULL)
 {
-	fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
-	fclose(bus.file);
-	free(bus.content);
-	free_stack(*head);
-	exit(EXIT_FAILURE);
-	}
-	h = *head;
-	*head = h->next;
-	free(h);
+fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
+fclose(bus.file);
+free(bus.content);
+free_stack(*head);
+exit(EXIT_FAILURE);
+}
+h = *head;
+*head = h->next;
+free(h);
 }
